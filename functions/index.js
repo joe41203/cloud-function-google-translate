c24b360dc2c2367616c886699b5879d4c27218cf
joe <kinjoyuji@gmail.com>
2019-02-06 01:43:28 +0900

    Hello World

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,36 @@
 const functions = require('firebase-functions');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+const { Translate } = require('@google-cloud/translate');
+
+const Config = require('./config.js');
+
+// Your Google Cloud Platform project ID
+const projectId = Config.projectId;
+
+// Instantiates a client
+const translate = new Translate({
+  projectId: projectId
+});
+
+const text = 'Hello, world!';
+// The target language
+const target = 'ja';
+
+// Translates some text into Russian
+exports.translate = functions.https.onRequest((request, response) => {
+  // const word = request.body['word'];
+  // const language = request.body['language'] || 'ja';
+  translate
+    .translate(text, target)
+    .then(results => {
+      const translation = results[0];
+
+      console.log(`Text: ${text}`);
+      console.log(`Translation: ${translation}`);
+
+      return response.send(`${text}: ${translation}`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 });
