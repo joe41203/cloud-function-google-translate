dad874f8b06a1cf8263eb0de1a7eb0d84f6007dc
joe <kinjoyuji@gmail.com>
2019-02-06 02:35:33 +0900

    fix

--- functions/index.js
+++ functions/index.js
@@ -12,19 +12,17 @@ const translate = new Translate({
   projectId: projectId
 });
 
-const text = 'Hello, world!';
-// The target language
-const target = 'ja';
-
-// Translates some text into Russian
 exports.translate = functions.https.onRequest((request, response) => {
-  // const word = request.body['word'];
-  // const language = request.body['language'] || 'ja';
+  const text = request.query['text'];
+  const target = 'ja';
+
   translate
     .translate(text, target)
     .then(results => {
       const translation = results[0];
 
+      console.dir(results);
+      console.log(JSON.stringify(results));
       console.log(`Text: ${text}`);
       console.log(`Translation: ${translation}`);
 
